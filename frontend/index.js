2b4fccf887235976f1c7e1e0a738820546c28fee
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-02-19 11:04:25 +0200

    update home page

--- frontend/index.js
+++ frontend/index.js
@@ -7,11 +7,13 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
         // if the nav is closed, open it
         const visibility = nav.getAttribute("data-visible");
         if(visibility === "false"){
-            nav.setAttribute("data-visible", true)
+            nav.setAttribute("data-visible", true);
+            navToggle.setAttribute("aria-expanded", true);
         }
         // if the nav is open, close it
         else {
-            nav.setAttribute("data-visible", false)
+            nav.setAttribute("data-visible", false);
+            navToggle.setAttribute("aria-expanded", false);
         }
         // console.log(visibility)
-    })
+    });
