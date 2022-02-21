98ee64cd726455e60c482b92f6256a150d6794ea
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-02-21 18:35:06 +0200

    on change tab panel

--- frontend/tabs.js
+++ frontend/tabs.js
@@ -1,14 +1,20 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown',onChangeTabFocus)
+
+tabs.forEach((tab)=>{
+    tab.addEventListener('click',onchangeTabPanel)
+});
 let tabFocus = 0;
-tabList.addEventListener('keydown', (e)=>{
+function onChangeTabFocus(e){
     const keydownLeft = 37;
     const keydownRight = 39;
     // when we change the tabindex of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight){
         tabs[tabFocus].setAttribute('tabindex', -1);
     }
-    // if the right key is pushed, move to the next tab on the right
+        // if the right key is pushed, move to the next tab on the right
     if(e.keyCode === keydownRight){
         tabFocus++;
         if(tabFocus >= tabs.length){
@@ -24,4 +30,12 @@ tabList.addEventListener('keydown', (e)=>{
     }
     tabs[tabFocus].setAttribute("tabindex", 0);
     tabs[tabFocus].focus();
-})
+}
+
+function onchangeTabPanel(e){
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const tabContainer =targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+}
