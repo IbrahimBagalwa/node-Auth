f24c2698dfa110dfcb006b3aa4f7251972bdaa97
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-02-24 09:26:41 +0200

    update

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const nav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
+// const nav = document.querySelector(".primary-navigation");
+// const navToggle = document.querySelector(".mobile-nav-toggle");
 
-// when someone clicks the hamburger button
-    navToggle.addEventListener("click", ()=>{
+// // when someone clicks the hamburger button
+//     navToggle.addEventListener("click", ()=>{
 
-        // if the nav is closed, open it
-        const visibility = nav.getAttribute("data-visible");
-        if(visibility === "false"){
-            nav.setAttribute("data-visible", true);
-            navToggle.setAttribute("aria-expanded", true);
+//         // if the nav is closed, open it
+//         const visibility = nav.getAttribute("data-visible");
+//         if(visibility === "false"){
+//             nav.setAttribute("data-visible", true);
+//             navToggle.setAttribute("aria-expanded", true);
+//         }
+//         // if the nav is open, close it
+//         else {
+//             nav.setAttribute("data-visible", false);
+//             navToggle.setAttribute("aria-expanded", false);
+//         }
+//         // console.log(visibility)
+//     });
+
+    function find_max(nums){
+        let max_num =Number.NEGATIVE_INFINITY;
+        for(let num of nums){
+            if(num > max_num){
+                max_num += 1;
+            }
         }
-        // if the nav is open, close it
-        else {
-            nav.setAttribute("data-visible", false);
-            navToggle.setAttribute("aria-expanded", false);
-        }
-        // console.log(visibility)
-    });
+        return max_num
+    }
+    console.log(find_max(20));
